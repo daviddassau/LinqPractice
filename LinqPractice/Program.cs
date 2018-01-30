6bf3e08847bdc82a8af5e76b18622c870e1c606e
David Dassau <todieahero@hotmail.com>
2018-01-30 11:10:19 -0600

    finished linq practice

---
+++ LinqPractice/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            var LFruits =
+                from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            foreach (var x in LFruits)
+            {
+                Console.WriteLine(x);
+            }
+
+
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var fourSixMultiples = numbers.Where(x => x % 1 == 0);
+
+            Console.WriteLine(fourSixMultiples);
+
+
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            var descend =
+                from name in names
+                orderby name descending
+                select name;
+
+            Console.WriteLine("Student names in reverse alpha order:");
+            foreach (var namesInReverse in descend)
+            {
+                Console.WriteLine(namesInReverse);
+            }
+
+
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var numbersInOrder =
+                from number in numbers
+                orderby number ascending
+                select number;
+
+            foreach (var boobHat in numbersInOrder)
+            {
+                Console.WriteLine(boobHat);
+            }
+
+
+
+            // Output how many numbers are in this list
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var howManyNumbers = numbers.Count();
+
+            Console.WriteLine(howManyNumbers);
+
+
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            double numSum = purchases.Sum();
+            Console.WriteLine($"We have made {numSum}.");
+
+
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            double maxNum = prices.Max();
+            Console.WriteLine($"Our most expensive product costs {maxNum}");
+
+
+
+            //Store each number in the following List until a perfect square is detected
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+
+
+            // Build a collection of customers who are millionaires
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            var millionaires =
+                from balance in customers
+                where balance.Balance >= 1000000
+                select balance;
+
+            Console.WriteLine("Here's a list of customers that are millionaires");
+            foreach (var millName in millionaires)
+            {
+                Console.WriteLine(millName.Name);
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
